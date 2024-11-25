827b2eb3f3cb4f870e46f64c88d1008b2aff1966
Charantej Komaram <86775300+Charan00732555@users.noreply.github.com>
2024-11-25 22:31:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const secondsEl = document.getElementById("seconds");
 
 const newYears = "9 Dec 2024 17:00:00";
 
+// Array of background images
+const backgrounds = [
+   './BG_1.jpg',
+   './BG_2.jpg',
+   './BG_3.jpg',
+   './BG_4.png',
+   './BG_5.jpg',
+   './BG_6.jpg'
+];
+
+let backgroundIndex = 0;
+
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
@@ -26,8 +38,20 @@ function formatTime(time) {
     return time < 10 ? `0${time}` : time;
 }
 
-// Initial call
+function changeBackground() {
+    // Increment the index and loop back to the start if needed
+    backgroundIndex = (backgroundIndex + 1) % backgrounds.length;
+
+    // Set the new background
+    document.body.style.backgroundImage = `url('${backgrounds[backgroundIndex]}')`;
+}
+
+// Initial calls
 countdown();
+changeBackground();
 
 // Update the countdown every second
 setInterval(countdown, 1000);
+
+// Update the background every 10 seconds
+setInterval(changeBackground, 10000);
